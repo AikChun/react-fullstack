7f443a9b67ae0b899384f934ccc1250a0c0a50b8
aikchun <aikchun616@gmail.com>
2018-08-30 21:17:41 +0800

    Defined serializeUser and deserializeUser of passport

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -13,17 +23,17 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (!existingUser) {
-          new User({ googleId: profile.id }).save().then(user =>
-            done(null, user)
-          );
-        } else {
-          done(null, existingUser);
-        }
-      }).catch(() => {
-        
-      });
+      User.findOne({ googleId: profile.id })
+        .then(existingUser => {
+          if (!existingUser) {
+            new User({ googleId: profile.id })
+              .save()
+              .then(user => done(null, user));
+          } else {
+            done(null, existingUser);
+          }
+        })
+        .catch(() => {});
     }
   )
 );
