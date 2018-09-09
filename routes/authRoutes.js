a9770b7c837f2801122399e44f749312712a21dc
aikchun <aikchun616@gmail.com>
2018-09-09 12:30:28 +0800

    Changed route for logout to be app.get instead of app.use

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,13 +2,13 @@ const passport = require('passport');
 module.exports = app => {
   app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-  app.use('/api/logout', (req, res) => {
-    req.logout();
+  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
-  app.get('/api/current_user', (req, res) => {
+  app.get('/api/logout', (req, res) => {
+    req.logout();
     res.send(req.user);
   });
 };
