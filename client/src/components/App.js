b8277795f325a6118a6ba8faac6a32232a1af462
aikchun <aikchun616@gmail.com>
2018-09-13 17:20:02 +0800

    Added materialize-css Changed Header styling

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-
-const Header = () => <h2>Header</h2>;
+import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>Survey New</h2>;
 const Landing = () => <h2>Landing</h2>;
@@ -10,7 +9,7 @@ const App = () => {
     <div>
       <BrowserRouter>
         <Route>
-          <div>
+          <div className="container">
             <Header />
             <Route exact path="/" component={Landing} />
             <Route exact path="/surveys" component={Dashboard} />
