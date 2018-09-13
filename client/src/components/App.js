8728c8df14c993a1d664d92cce21b0b7b365c2c2
aikchun <aikchun616@gmail.com>
2018-09-13 17:50:32 +0800

    Refactoed App component to class based component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>Survey New</h2>;
 const Landing = () => <h2>Landing</h2>;
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <Route>
-          <div className="container">
-            <Header />
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/surveys" component={Dashboard} />
-            <Route path="/surveys/new" component={SurveyNew} />
-          </div>
-        </Route>
-      </BrowserRouter>
-    </div>
-  );
-};
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Route>
+            <div className="container">
+              <Header />
+              <Route exact path="/" component={Landing} />
+              <Route exact path="/surveys" component={Dashboard} />
+              <Route path="/surveys/new" component={SurveyNew} />
+            </div>
+          </Route>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
 export default App;
