7e82207db2221f2731636a3b410cc68459df5d71
aikchun <aikchun616@gmail.com>
2018-09-13 17:59:03 +0800

    Wired up fetchUser with App component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>Survey New</h2>;
 const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
   render() {
     return (
       <div>
@@ -24,4 +30,7 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(
+  null,
+  actions
+)(App);
